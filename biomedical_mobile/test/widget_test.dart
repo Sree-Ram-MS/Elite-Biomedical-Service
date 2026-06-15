74b5bd678184cc3ce00292f0154e0ec45a66a954
Sree Ram M S <srmsm3551@gmail.com>
2026-06-15 17:46:05 +0530

    Remove biomedical_mobile from tracking (moving to submodule)

--- biomedical_mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:biomedical_mobile/main.dart';
-
-void main() {
-  testWidgets('Smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const BiomedicalApp());
-    expect(find.text('ELITE BIOMEDICAL'), findsOneWidget);
-  });
-}
